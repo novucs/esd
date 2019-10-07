9b9798f822d23b60009bb75eaaa8c8abd790326c
novucs <contact@novucs.net>
2019-10-07 17:54:44 +0100

    Remove dependency on inotify-tools
    Hopefully this will improve compatibility with Macs...

--- scripts/autodeploy.sh
+++ scripts/autodeploy.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
-if ! [ -x "$(command -v inotifywait)" ]; then
-  echo 'Error: inotify tools must be installed' >&2
-  exit 1
-fi
-
-PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
+PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)/.."
 cd "${PROJECT_DIR}" || exit
 docker-compose up -d
 trap 'trap - SIGTERM && kill -- -$$' SIGINT SIGTERM EXIT
+./gradlew build
 ./gradlew war -t &
 
-while (inotifywait -qq -e modify ./build/libs/esd*.war); do
-  docker cp ./build/libs/*.war "$(docker-compose ps -q app)":/app/glassfish/domains/domain1/autodeploy/app.war
+WARFILE="./build/libs/*.war"
+
+function date_of_warfile() {
+    return "$(stat -c %y "${WARFILE}")"
+}
+
+last_modified=date_of_warfile
+while sleep 1; do
+  if [[ $last_modified != date_of_warfile ]]; then
+    last_modified=date_of_warfile
+    docker cp ./build/libs/*.war "$(docker-compose ps -q app)":/app/glassfish/domains/domain1/autodeploy/app.war
+  fi
 done
