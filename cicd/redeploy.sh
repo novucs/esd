fa005d0924bc37ba106b9c33f50145628094d6f7
novucs <contact@novucs.net>
2019-10-14 18:59:57 +0100

    Redploy script

--- cicd/redeploy.sh
+++ cicd/redeploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit 1
-docker cp "$(docker-compose ps -q jenkins)":"$1"/build/libs/*.war /tmp/"$2".war
+WARFILE=$(docker-compose exec jenkins find "$1"/build/libs | grep war | head -n 1)
+docker cp "$(docker-compose ps -q jenkins)":"$WARFILE" /tmp/"$2".war
 docker cp /tmp/"$2".war "$(docker-compose ps -q app)":/app/glassfish/domains/domain1/autodeploy/"$2".war
