7ac6f4090513b93eb2441cfa5f16f81ad0449bdf
novucs <contact@novucs.net>
2019-10-14 19:40:09 +0100

    Fix stupid docker-compose bug

--- cicd/redeploy.sh
+++ cicd/redeploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit 1
-WARFILE=$(docker-compose exec jenkins find "$1"/build/libs | grep war | head -n 1)
-docker cp "$(docker-compose ps -q jenkins)":"$WARFILE" /tmp/"$2".war
+# tr -d '\r' resolves installation specifc docker-compose bug with non-unix line endings
+# this took too long to debug...
+WARFILE="$(docker-compose ps -q jenkins):$(docker-compose exec jenkins find "$1"/build/libs | grep war | head -n 1 | tr -d '\r')"
+docker cp "${WARFILE}" /tmp/"$2".war
 docker cp /tmp/"$2".war "$(docker-compose ps -q app)":/app/glassfish/domains/domain1/autodeploy/"$2".war
