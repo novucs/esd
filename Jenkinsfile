d1795205cc607dd8e9ff2c94280f2c522080f1f4
novucs <contact@novucs.net>
2019-10-14 20:29:42 +0100

    Add spot bug checks to ci

--- Jenkinsfile
+++ Jenkinsfile
@@ -11,6 +11,15 @@ pipeline {
                 sh './gradlew autodeploy'
             }
         }
+        stage('run all tests') {
+            parallel {
+                stage('spot bugs') {
+                    steps {
+                        sh './gradlew check'
+                    }
+                }
+            }
+        }
         stage('deploy') {
             steps {
                 sh "ssh billy@esd.novucs.net ./esd/cicd/redeploy.sh ${pwd()} app"
--- src/main/java/net/novucs/esd/Main.java
+++ src/main/java/net/novucs/esd/Main.java
@@ -5,42 +5,58 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Map<String, String> env = System.getenv();
-        String dbHost = env.getOrDefault("DB_HOST", "localhost");
-        String dbPort = env.getOrDefault("DB_PORT", "1527");
-        String dbUser = env.getOrDefault("DB_USER", "impact");
-        String dbPass = env.getOrDefault("DB_PASS", "derbypass");
-        Connection con = DriverManager.getConnection(
-                String.format("jdbc:derby://%s:%s/esd;create=true", dbHost, dbPort),
-                dbUser, dbPass);
-
+        Statement statement = null;
+        ResultSet resultSet = null;
+        Connection con = null;
+        PreparedStatement createUserTable = null;
         try {
-            PreparedStatement createUserTable = con.prepareStatement(
-                    "CREATE TABLE person (" +
-                            "id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), " +
-                            "name VARCHAR(255), " +
-                            "PRIMARY KEY (id))");
-            createUserTable.execute();
-        } catch (SQLException ignore) {
-        }
+            Map<String, String> env = System.getenv();
+            String dbHost = env.getOrDefault("DB_HOST", "localhost");
+            String dbPort = env.getOrDefault("DB_PORT", "1527");
+            String dbUser = env.getOrDefault("DB_USER", "impact");
+            String dbPass = env.getOrDefault("DB_PASS", "derbypass");
+            con = DriverManager.getConnection(
+                    String.format("jdbc:derby://%s:%s/esd;create=true", dbHost, dbPort),
+                    dbUser, dbPass);
 
-        Statement statement = con.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM person");
-        ResultSetMetaData metaData = resultSet.getMetaData();
-        int numberOfColumns = metaData.getColumnCount();
-        for (int i = 1; i <= numberOfColumns; i++) {
-            System.out.print(metaData.getColumnName(i) + "\t");
-        }
-        System.out.println();
+            try {
+                createUserTable = con.prepareStatement(
+                        "CREATE TABLE person (" +
+                                "id INT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), " +
+                                "name VARCHAR(255), " +
+                                "PRIMARY KEY (id))");
+                createUserTable.execute();
+            } catch (SQLException ignore) {
+            }
 
-        while (resultSet.next()) {
-            for (int i = 1; i <= numberOfColumns; i++)
-                System.out.print(resultSet.getObject(i) + "\t");
+            statement = con.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM person");
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int numberOfColumns = metaData.getColumnCount();
+            for (int i = 1; i <= numberOfColumns; i++) {
+                System.out.print(metaData.getColumnName(i) + "\t");
+            }
             System.out.println();
+
+            while (resultSet.next()) {
+                for (int i = 1; i <= numberOfColumns; i++)
+                    System.out.print(resultSet.getObject(i) + "\t");
+                System.out.println();
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+            if (createUserTable != null) {
+                createUserTable.close();
+            }
+            System.out.println("Success");
         }
-        resultSet.close();
-        statement.close();
-        con.close();
-        System.out.println("Success");
     }
 }
