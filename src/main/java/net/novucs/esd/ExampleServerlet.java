4d73d15f7c589bee137d6e8bcd789a4f7b0fb4c0
novucs <contact@novucs.net>
2019-10-16 16:03:33 +0100

    Use relative paths
    ... and figure how to access the app name

--- src/main/java/net/novucs/esd/ExampleServerlet.java
+++ src/main/java/net/novucs/esd/ExampleServerlet.java
@@ -3,6 +3,7 @@ package net.novucs.esd;
 import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import javax.annotation.Resource;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,12 +13,16 @@ public class ExampleServerlet extends HttpServlet {
 
   private static final long serialVersionUID = 1426082847044519303L;
 
+  @Resource(lookup = "java:app/AppName")
+  private transient String appName;
+
   private void processRequest(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
     response.setContentType("text/html;charset=UTF-8");
     Map<String, String> anExampleMap = new ConcurrentHashMap<>();
     anExampleMap.put("a", "b");
     anExampleMap.put("hello", "friend");
+    anExampleMap.put("appname", this.appName);
     request.setAttribute("anExampleMap", anExampleMap);
     request.getRequestDispatcher("/example.jsp").forward(request, response);
   }
