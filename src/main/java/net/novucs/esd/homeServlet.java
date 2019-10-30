5572ec26d537b0cfdf41d78328daba0042615933
Chris Taylor <christopherjamestaylor@outlook.com>
2019-10-30 12:21:50 +0000

    Started on the homepage. Also working on the servlets to the members and admin dashboard.

---
+++ src/main/java/net/novucs/esd/homeServlet.java
@@ -0,0 +1,43 @@
+package net.novucs.esd;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class homeServlet extends HttpServlet {
+
+  private static final long serialVersionUID = 1426082847044519303L;
+
+  @Resource(lookup = "java:app/AppName")
+  private transient String appName;
+
+  private void processRequest(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    response.setContentType("text/html;charset=UTF-8");
+    Map<String, String> anExampleMap = new ConcurrentHashMap<>();
+    anExampleMap.put("appname", this.appName);
+    request.setAttribute("anExampleMap", anExampleMap);
+
+    // Setting the title and the location of the page you would like to view
+    // while dispatching to the layout will ensure consistent UI.
+    request.setAttribute("title", "Homepage");
+    request.setAttribute("page", "/homepage.jsp");
+    request.getRequestDispatcher("/layout.jsp").forward(request, response);
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    processRequest(request, response);
+  }
+
+  @Override
+  public String getServletInfo() {
+    return "Homepage is loaded";
+  }
+}
