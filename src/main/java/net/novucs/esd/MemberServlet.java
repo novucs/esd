32acc4550f3abf1a0c3741046b8ac2d9b96f89d5
Reece Benson <business@reecebenson.me>
2019-11-01 01:33:27 +0000

    Homepage & Members Page Implementation (#42)
    * Started on the homepage. Also working on the servlets to the members and admin dashboard.
    
    * Homepage Jenkins Fixes
    
    * Fix homepage HTML integrity
    
    * Cleanup Servlets and execute PR reviewal suggestions

---
+++ src/main/java/net/novucs/esd/MemberServlet.java
@@ -0,0 +1,34 @@
+package net.novucs.esd;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MemberServlet extends HttpServlet {
+
+  private static final long serialVersionUID = 1426082847044519303L;
+
+  private void processRequest(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    response.setContentType("text/html;charset=UTF-8");
+
+    // Setting the title and the location of the page you would like to view
+    // while dispatching to the layout will ensure consistent UI.
+    request.setAttribute("title", "Members");
+    request.setAttribute("page", "/members.jsp");
+    request.getRequestDispatcher("/layout.jsp").forward(request, response);
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    processRequest(request, response);
+  }
+
+  @Override
+  public String getServletInfo() {
+    return "Member Dashboard Servlet";
+  }
+}
