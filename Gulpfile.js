4d947c680e1ab1a2bbd200354c47e42bf586952e
Reece Benson <business@reecebenson.me>
2019-11-08 17:53:01 +0000

    Update Gulpfile to minify output CSS

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,7 +4,7 @@ var sass = require('gulp-sass');
 // Gulp Styles compiling
 gulp.task('default', function(done) {
   gulp.src('src/main/webapp/scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(gulp.dest('src/main/webapp/css/'));
   done();
 });
