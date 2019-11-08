92f36204a11f1830c3b0e9be1bd3e05c7dd3f24a
Reece Benson <business@reecebenson.me>
2019-11-08 18:00:56 +0000

    Implement SASS Compiling (...again) (#86)
    * Move to SASS Compiling, resolves #24
    
    * Removal of non-git wanted files
    
    * Improve the Gulp workflow; no longer has to adapt the Docker instances to SCSS compile. README update.
    
    * Re-add the removed files to match current state of master
    
    * Update Gulpfile to minify output CSS
    
    * Refactor site.css to match master (no idea why it wasn't)

---
+++ Gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+// Gulp Styles compiling
+gulp.task('default', function(done) {
+  gulp.src('src/main/webapp/scss/*.scss')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(gulp.dest('src/main/webapp/css/'));
+  done();
+});
