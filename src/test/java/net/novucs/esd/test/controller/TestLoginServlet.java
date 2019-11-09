9a733e33e8aaa2b79d9693c73a1d156a8e7f4a09
Lewis Cummins <lewisjcummins@hotmail.co.uk>
2019-11-09 18:50:26 +0000

    Feature/login tests attempt 2 (#90)
    * Finish GET login page and successful login  test
    
    * Finish login unit tests
    
    * Fix null pointer issues
    
    * Attempt 2 at login tests
    
    * Changed the test to verify that the errors are pushed to the session.
    
    * Fixed checkstyle issues

---
+++ src/test/java/net/novucs/esd/test/controller/TestLoginServlet.java
@@ -0,0 +1,175 @@
+package net.novucs.esd.test.controller;
+
+import static net.novucs.esd.test.util.TestUtils.createTestDaoManager;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.ZonedDateTime;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import net.novucs.esd.controllers.LoginServlet;
+import net.novucs.esd.lifecycle.DatabaseLifecycle;
+import net.novucs.esd.lifecycle.Session;
+import net.novucs.esd.model.User;
+import net.novucs.esd.orm.Dao;
+import net.novucs.esd.orm.DaoManager;
+import net.novucs.esd.orm.Where;
+import net.novucs.esd.util.Password;
+import net.novucs.esd.util.ReflectUtil;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.stubbing.Answer;
+
+public class TestLoginServlet {
+
+  private static final String LAYOUT_PAGE = "/layout.jsp";
+
+  private static final String SESSION =  "session";
+
+  @Test
+  public void testRequestGetsLoginPage() throws ServletException, IOException {
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    when(request.getRequestDispatcher(LAYOUT_PAGE)).thenAnswer(
+        (Answer<RequestDispatcher>) invocation -> mock(RequestDispatcher.class));
+    when(request.getSession(anyBoolean())).thenReturn(mock(HttpSession.class));
+
+    LoginServlet servlet = new LoginServlet();
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    servlet.doGet(request, response);
+    verify(request).setAttribute("page", "/login.jsp");
+    verify(request).getRequestDispatcher(LAYOUT_PAGE);
+  }
+
+  @Test
+  public void testLoginSuccess()
+      throws SQLException, ReflectiveOperationException, ServletException, IOException {
+    DaoManager dm = createTestDaoManager();
+    dm.init(DatabaseLifecycle.MODEL_CLASSES);
+    Dao<User> userDao = dm.get(User.class);
+    // Create test user
+    User userToCreate = new User(
+        "LoginTestUser1",
+        "user@test.com",
+        Password.fromPlaintext("testPassword"),
+        "House,A Street,A city,County,AB12 C34",
+        ZonedDateTime.now(),
+        "APPLICATION"
+    );
+
+    userDao.insert(userToCreate);
+
+    LoginServlet loginServlet = new LoginServlet();
+    ReflectUtil.setFieldValue(loginServlet, "userDao", userDao);
+    HttpServletRequest request = mock(HttpServletRequest.class);
+
+    User userToLogin = userDao.select().where(new Where().eq("name", "LoginTestUser1")).first();
+    String password = "testPassword";
+
+    when(request.getParameter("username")).thenReturn(userToLogin.getEmail());
+    when(request.getParameter("password")).thenReturn(password);
+    HttpSession session = mock(HttpSession.class);
+    when(request.getSession(anyBoolean())).thenReturn(session);
+    when(request.getRequestDispatcher(any())).thenReturn(mock(RequestDispatcher.class));
+    when(session.getAttribute(eq(SESSION))).thenReturn(null);
+
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    loginServlet.doPost(request, response);
+
+    ArgumentCaptor<Session> argument = ArgumentCaptor.forClass(Session.class);
+    verify(session).setAttribute(eq(SESSION), argument.capture());
+    verify(response).sendRedirect("homepage");
+
+    assertEquals("Has user been correctly stored in session.",
+        userToLogin.getId(), argument.getValue().getUser().getId());
+  }
+
+  @Test
+  public void testPasswordIncorrect()
+      throws SQLException, ReflectiveOperationException, ServletException, IOException {
+    DaoManager dm = createTestDaoManager();
+    dm.init(DatabaseLifecycle.MODEL_CLASSES);
+    Dao<User> userDao = dm.get(User.class);
+
+    User userToCreate = new User(
+        "LoginTestUser2",
+        "user@test.com",
+        Password.fromPlaintext("pass"),
+        "House,A Street,A city,County,AB12 C34",
+        ZonedDateTime.now(),
+        "APPLICATION"
+    );
+    userDao.insert(userToCreate);
+
+    LoginServlet loginServlet = new LoginServlet();
+    ReflectUtil.setFieldValue(loginServlet, "userDao", userDao);
+    HttpServletRequest request = mock(HttpServletRequest.class);
+
+    User userToLogin = userDao.select().where(new Where().eq("name", "LoginTestUser2")).first();
+    String incorrectPassword = "incorrectPassword";
+
+    when(request.getParameter("username")).thenReturn(userToLogin.getEmail());
+    when(request.getParameter("password")).thenReturn(incorrectPassword);
+
+    HttpSession session = mock(HttpSession.class);
+    Session esdSession = mock(Session.class);
+
+    when(request.getSession(anyBoolean())).thenReturn(session);
+    when(request.getRequestDispatcher(any())).thenReturn(mock(RequestDispatcher.class));
+    when(session.getAttribute(SESSION)).thenReturn(esdSession);
+
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    loginServlet.doPost(request, response);
+
+    verify(request).getRequestDispatcher(LAYOUT_PAGE);
+    verify(esdSession).pushError("Incorrect username or password");
+  }
+
+  @Test
+  public void testUsernameIncorrect()
+      throws SQLException, ReflectiveOperationException, ServletException, IOException {
+    DaoManager dm = createTestDaoManager();
+    dm.init(DatabaseLifecycle.MODEL_CLASSES);
+    Dao<User> userDao = dm.get(User.class);
+
+    User userToCreate = new User(
+        "LoginTestUser3",
+        "user@test.com",
+        Password.fromPlaintext("correctPassword"),
+        "House,A Street,A city,County,AB12 C34",
+        ZonedDateTime.now(),
+        "APPLICATION"
+    );
+    userDao.insert(userToCreate);
+
+    LoginServlet loginServlet = new LoginServlet();
+    ReflectUtil.setFieldValue(loginServlet, "userDao", userDao);
+    HttpServletRequest request = mock(HttpServletRequest.class);
+
+    String password = "correctPassword";
+
+    when(request.getParameter("username")).thenReturn("InvalidEmail@thisshouldnotwork.com");
+    when(request.getParameter("password")).thenReturn(password);
+
+    HttpSession session = mock(HttpSession.class);
+    Session esdSession = mock(Session.class);
+
+    when(request.getSession(anyBoolean())).thenReturn(session);
+    when(request.getRequestDispatcher(any())).thenReturn(mock(RequestDispatcher.class));
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    when(session.getAttribute(eq(SESSION))).thenReturn(esdSession);
+
+    loginServlet.doPost(request, response);
+    verify(request).getRequestDispatcher(LAYOUT_PAGE);
+    verify(esdSession).pushError("Incorrect username or password");
+  }
+}
