07339f4633a7d5ff577419d4c8ea1faca1d316cc
Reece Benson <reece@circle-interactive.co.uk>
2019-11-09 16:43:22 +0000

    100% Coverage on Admin / Member Servlets, to be refactored

--- src/test/java/net/novucs/esd/test/controller/member/TestMemberMakePaymentServlet.java
+++ src/test/java/net/novucs/esd/test/controller/member/TestMemberMakePaymentServlet.java
@@ -1,21 +1,25 @@
 package net.novucs.esd.test.controller.member;
 
 import static junit.framework.TestCase.assertTrue;
-import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
-import java.util.Map;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import net.novucs.esd.controllers.member.MemberMakePaymentServlet;
+import net.novucs.esd.lifecycle.Session;
+import net.novucs.esd.model.User;
+import net.novucs.esd.util.DateUtil;
+import net.novucs.esd.util.Password;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.stubbing.Answer;
 
@@ -25,22 +29,57 @@ public class TestMemberMakePaymentServlet {
    TODO: Refactor this to test for the implementation of MemberMakePaymentServlet
    */
 
+  private transient Session userSession;
+
+  @Before
+  public void initialiseTest() {
+    userSession = new Session();
+    userSession.setUser(new User(
+        "testuser",
+        "testuser@example.com",
+        Password.fromPlaintext("test_pass"),
+        "Line 1,Line 2,City,County,Postcode",
+        new DateUtil().getDateFromString("2000-01-01"),
+        "APPLICATION"
+    ));
+  }
+
   @Test
   public void testRequestGetsMapAttribute()
       throws ServletException, IOException {
     // Given
     MemberMakePaymentServlet servlet = new MemberMakePaymentServlet();
-    HttpServletRequest request = mock(HttpServletRequest.class);
     HttpServletResponse response = mock(HttpServletResponse.class);
+    HttpSession httpSession = mock(HttpSession.class);
+    HttpServletRequest request = mock(HttpServletRequest.class);
 
     // When
+    when(httpSession.getAttribute(eq("session"))).thenReturn(userSession);
     when(request.getRequestDispatcher("/layout.jsp")).thenAnswer(
         (Answer<RequestDispatcher>) invocation -> mock(RequestDispatcher.class));
-    when(request.getSession(anyBoolean())).thenReturn(mock(HttpSession.class));
+    when(request.getSession(anyBoolean())).thenReturn(httpSession);
     servlet.doGet(request, response);
 
     // Assert
-    verify(request, times(0)).setAttribute(any(String.class), any(Map.class));
+    verify(request).getRequestDispatcher(eq("/layout.jsp"));
+  }
+
+  @Test
+  public void testRequestHasNoSession()
+      throws ServletException, IOException {
+    // Given
+    MemberMakePaymentServlet servlet = new MemberMakePaymentServlet();
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    HttpSession httpSession = mock(HttpSession.class);
+    HttpServletRequest request = mock(HttpServletRequest.class);
+
+    // When
+    when(request.getSession(anyBoolean())).thenReturn(httpSession);
+    when(httpSession.getAttribute(eq("session"))).thenReturn(null);
+    servlet.doGet(request, response);
+
+    // Assert
+    verify(response, times(1)).sendRedirect("login");
   }
 
   @Test
