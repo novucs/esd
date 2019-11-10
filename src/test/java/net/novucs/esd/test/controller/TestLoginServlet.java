644dd831bdfffcba36f383890d2da00a5b9cb129
novucs <contact@novucs.net>
2019-11-10 17:20:21 +0000

    Fix login servlet test

--- src/test/java/net/novucs/esd/test/controller/TestLoginServlet.java
+++ src/test/java/net/novucs/esd/test/controller/TestLoginServlet.java
@@ -4,8 +4,10 @@ import static net.novucs.esd.test.util.TestUtils.createTestDaoManager;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -34,7 +36,7 @@ public class TestLoginServlet {
 
   private static final String LAYOUT_PAGE = "/layout.jsp";
 
-  private static final String SESSION =  "session";
+  private static final String SESSION = "session";
 
   @Test
   public void testRequestGetsLoginPage() throws ServletException, IOException {
@@ -86,11 +88,10 @@ public class TestLoginServlet {
     loginServlet.doPost(request, response);
 
     ArgumentCaptor<Session> argument = ArgumentCaptor.forClass(Session.class);
-    verify(session).setAttribute(eq(SESSION), argument.capture());
-    verify(response).sendRedirect(".");
+    verify(session, times(2)).setAttribute(eq(SESSION), argument.capture());
 
     assertEquals("Has user been correctly stored in session.",
-        userToLogin.getId(), argument.getValue().getUser().getId());
+        userToLogin.getId(), argument.getAllValues().get(0).getUser().getId());
   }
 
   @Test
