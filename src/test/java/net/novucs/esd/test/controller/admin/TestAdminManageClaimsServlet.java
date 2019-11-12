a9fddbbdbe47b0df22e2fb66b345a6bbf4ddaddb
Reece Benson <business@reecebenson.me>
2019-11-12 20:34:45 +0000

    Fix PMD

--- src/test/java/net/novucs/esd/test/controller/admin/TestAdminManageClaimsServlet.java
+++ src/test/java/net/novucs/esd/test/controller/admin/TestAdminManageClaimsServlet.java
@@ -4,7 +4,6 @@ import static junit.framework.TestCase.assertTrue;
 import static org.mockito.Matchers.anyBoolean;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
