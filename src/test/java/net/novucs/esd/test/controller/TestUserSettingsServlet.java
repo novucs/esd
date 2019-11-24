6ef5654eff7a06975f0202f8094c7df04e2b8e6a
Chris Taylor <christopherjamestaylor@outlook.com>
2019-11-24 16:12:35 +0000

    Users: Account Settings (#143)
    * A start on the user settings.
    
    * A start on the user settings.
    
    * Role has been implemented.
    
    * I have updated the servlet to post the user's updated details. I am yet to implement the post for password reset.
    
    * Editing user's basic info has been implemented and tested
    
    * I have created a password reset function with ui. However, I can not verify the current password against the database because the from hash set doesn't decode the password.
    
    * PMD changes.
    
    * This allows verification of current password and the password typed in.
    
    * I have implemented the review changes from the PR.
    
    * I have implemented the review changes from the PR.
    
    * I have added the success page and the user's role header.
    
    * The user now gets redirected if there is a sql error within the user settings.
    
    * I have removed the error page as I was incorrect in my assumption for its
    need.
    
    * I have implemented the PR requests.
    
    * Tests for user settings have been implemented.
    
    * Test PR changes.
    
    * Optimise tests for correctly checking user password update state
    
    * Fix PMD

--- src/test/java/net/novucs/esd/test/controller/TestUserSettingsServlet.java
+++ src/test/java/net/novucs/esd/test/controller/TestUserSettingsServlet.java
@@ -1,57 +1,235 @@
 package net.novucs.esd.test.controller;
 
 import static junit.framework.TestCase.assertTrue;
+import static net.novucs.esd.test.util.TestUtils.createTestDaoManager;
 import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyList;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import net.novucs.esd.controllers.UserSettingsServlet;
+import net.novucs.esd.lifecycle.DatabaseLifecycle;
 import net.novucs.esd.lifecycle.Session;
+import net.novucs.esd.model.Role;
+import net.novucs.esd.model.User;
+import net.novucs.esd.model.UserRole;
+import net.novucs.esd.orm.Dao;
+import net.novucs.esd.orm.DaoManager;
 import net.novucs.esd.test.TestDummyDataUtils;
+import net.novucs.esd.util.ReflectUtil;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.stubbing.Answer;
 
 public class TestUserSettingsServlet {
 
-  /*
-   TODO: Refactor this to test for the implementation of UserSettingsServlet
-   */
+  private static final String SESSION_LABEL = "session";
+  private static final String USER_ID_LABEL = "userId";
+  private static final String ERRORS_LABEL = "errors";
+  private static final String LAYOUT_JSP_LABEL = "/layout.jsp";
 
   private transient Session userSession;
+  private transient Role userRole;
 
   @Before
   public void initialiseTest() {
     userSession = new Session();
-    userSession.setUser(TestDummyDataUtils.getDummyUser());
+    userSession.setUser(TestDummyDataUtils.getDummyAdminUser());
+    userRole = new Role("User");
+  }
+
+  private void setServletDaos(UserSettingsServlet servlet)
+      throws ReflectiveOperationException, SQLException {
+    DaoManager dm = createTestDaoManager();
+    dm.init(DatabaseLifecycle.MODEL_CLASSES);
+    Dao<User> userDao = dm.get(User.class);
+    Dao<UserRole> userRoleDao = dm.get(UserRole.class);
+    Dao<Role> roleDao = dm.get(Role.class);
+
+    // Insert our Dummy User with Roles
+    roleDao.insert(userRole);
+    userDao.insert(userSession.getUser());
+    userRoleDao.insert(
+        new UserRole(userSession.getUser().getId(), userRole.getId())
+    );
+
+    // Reflect DAO
+    ReflectUtil.setFieldValue(servlet, "userDao", userDao);
   }
 
   @Test
-  public void testRequestGetsMapAttribute()
-      throws ServletException, IOException {
+  public void testUpdateInvalidUser()
+      throws IOException, ServletException, SQLException, ReflectiveOperationException {
     // Given
     UserSettingsServlet servlet = new UserSettingsServlet();
     HttpServletResponse response = mock(HttpServletResponse.class);
-    HttpSession httpSession = mock(HttpSession.class);
     HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpSession session = mock(HttpSession.class);
+    setServletDaos(servlet);
+
+    // Empty Session
+    Session emptySession = new Session();
+    emptySession.setUser(null);
 
     // When
-    when(httpSession.getAttribute(eq("session"))).thenReturn(userSession);
-    when(request.getRequestDispatcher("/layout.jsp")).thenAnswer(
+    when(request.getSession(anyBoolean())).thenReturn(session);
+    when(session.getAttribute(SESSION_LABEL)).thenReturn(emptySession);
+    when(request.getRequestDispatcher(LAYOUT_JSP_LABEL)).thenAnswer(
         (Answer<RequestDispatcher>) invocation -> mock(RequestDispatcher.class));
-    when(request.getSession(anyBoolean())).thenReturn(httpSession);
+    servlet.doPost(request, response);
+
+    // Verify
+    verify(request).setAttribute(eq("error"), anyString());
+    verify(response).sendError(anyInt());
+  }
+
+  @Test
+  public void testUpdateUserDetails()
+      throws IOException, ServletException, SQLException, ReflectiveOperationException {
+    // Given
+    UserSettingsServlet servlet = new UserSettingsServlet();
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpSession session = mock(HttpSession.class);
+    setServletDaos(servlet);
+
+    // When
+    when(request.getSession(anyBoolean())).thenReturn(session);
+    when(session.getAttribute(eq(SESSION_LABEL))).thenReturn(userSession);
+    when(request.getParameter(eq("fullname"))).thenReturn("Test User");
+    when(request.getParameter(eq("email"))).thenReturn(
+        "test@esd.net");
+    when(request.getParameter(eq("address"))).thenReturn(
+        "1 ESD Lane");
+    when(request.getParameter(eq("date_of_birth"))).thenReturn(
+        "1970-01-01");
+    when(request.getParameter(eq("current_password"))).thenReturn("");
+    when(request.getParameter(eq("new_password"))).thenReturn("");
+    when(request.getRequestDispatcher(LAYOUT_JSP_LABEL)).thenAnswer(
+        (Answer<RequestDispatcher>) invocation -> mock(RequestDispatcher.class));
+    servlet.doPost(request, response);
+
+    // Verify
+    verify(request).setAttribute(eq(ERRORS_LABEL), anyList());
+  }
+
+  @Test
+  public void testUpdateUserPassword()
+      throws SQLException, ReflectiveOperationException, IOException, ServletException {
+    // Given
+    UserSettingsServlet servlet = new UserSettingsServlet();
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpSession session = mock(HttpSession.class);
+    setServletDaos(servlet);
+    String[] roles = {userRole.getId().toString()};
+
+    // When
+    when(request.getSession(anyBoolean())).thenReturn(session);
+    when(session.getAttribute(eq(SESSION_LABEL))).thenReturn(userSession);
+    when(request.getParameter(eq("fullname"))).thenReturn(
+        "Test User");
+    when(request.getParameter(eq("email"))).thenReturn(
+        "test@esd.net");
+    when(request.getParameter(eq("address"))).thenReturn(
+        "1 ESD Lane");
+    when(request.getParameter(eq("date_of_birth"))).thenReturn(
+        "1970-01-01");
+    when(request.getParameter(eq("current_password"))).thenReturn("bob");
+    when(request.getParameter(eq("new_password"))).thenReturn("enterprise");
+    when(request.getParameterValues(eq("roles"))).thenReturn(roles);
+    when(request.getRequestDispatcher(LAYOUT_JSP_LABEL)).thenAnswer(
+        (Answer<RequestDispatcher>) invocation -> mock(RequestDispatcher.class));
+    servlet.doPost(request, response);
+
+    // Verify
+    verify(request).setAttribute(eq("updated"), eq(true));
+    verify(request).setAttribute(eq(ERRORS_LABEL), anyList());
+  }
+
+  @Test
+  public void testUpdateUserIncorrectRepeatedPassword()
+      throws SQLException, ReflectiveOperationException, IOException, ServletException {
+    // Given
+    UserSettingsServlet servlet = new UserSettingsServlet();
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpSession session = mock(HttpSession.class);
+    setServletDaos(servlet);
+
+    // When
+    when(request.getSession(anyBoolean())).thenReturn(session);
+    when(session.getAttribute(eq(SESSION_LABEL))).thenReturn(userSession);
+    when(request.getParameter(eq("fullname"))).thenReturn(
+        "Test User");
+    when(request.getParameter(eq("email"))).thenReturn(
+        "test@esd.net");
+    when(request.getParameter(eq("address"))).thenReturn(
+        "1 ESD Lane");
+    when(request.getParameter(eq("date_of_birth"))).thenReturn(
+        "1970-01-01");
+    when(request.getParameter(eq("current_password"))).thenReturn("enterprise");
+    when(request.getParameter(eq("new_password"))).thenReturn("depression");
+    when(request.getRequestDispatcher(LAYOUT_JSP_LABEL)).thenAnswer(
+        (Answer<RequestDispatcher>) invocation -> mock(RequestDispatcher.class));
+    servlet.doPost(request, response);
+
+    // Verify
+    verify(request).setAttribute(eq(ERRORS_LABEL), anyList());
+  }
+
+  @Test
+  public void testGetInvalidEditUser()
+      throws IOException, ServletException, SQLException, ReflectiveOperationException {
+    // Given
+    UserSettingsServlet servlet = new UserSettingsServlet();
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpSession session = mock(HttpSession.class);
+    setServletDaos(servlet);
+
+    // When
+    when(request.getSession(anyBoolean())).thenReturn(session);
+    when(session.getAttribute(eq(SESSION_LABEL))).thenReturn(userSession);
+    when(request.getParameter(eq(USER_ID_LABEL))).thenReturn("696969");
+    when(request.getRequestDispatcher(LAYOUT_JSP_LABEL)).thenAnswer(
+        (Answer<RequestDispatcher>) invocation -> mock(RequestDispatcher.class));
     servlet.doGet(request, response);
 
-    // Assert
-    verify(request).getRequestDispatcher(eq("/layout.jsp"));
+    // Verify
+    verify(request).setAttribute(eq(ERRORS_LABEL), anyList());
+  }
+
+  @Test
+  public void testGetEditUser()
+      throws IOException, ServletException, SQLException, ReflectiveOperationException {
+    // Given
+    UserSettingsServlet servlet = new UserSettingsServlet();
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpSession session = mock(HttpSession.class);
+    setServletDaos(servlet);
+
+    // When
+    when(request.getSession(anyBoolean())).thenReturn(session);
+    when(session.getAttribute(eq(SESSION_LABEL))).thenReturn(userSession);
+    when(request.getRequestDispatcher(LAYOUT_JSP_LABEL)).thenAnswer(
+        (Answer<RequestDispatcher>) invocation -> mock(RequestDispatcher.class));
+    servlet.doGet(request, response);
+
+    // Verify
+    verify(request).setAttribute(eq(ERRORS_LABEL), anyList());
   }
 
   @Test
