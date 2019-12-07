2d7f516a15c22d62a9ab44ff76c94a09aff51ee4
novucs <contact@novucs.net>
2019-12-07 23:15:55 +0000

    remove pagination specific js

--- src/main/webapp/js/admin.manageapplications.js
+++ src/main/webapp/js/admin.manageapplications.js
@@ -1,29 +1,13 @@
 (function () {
 
-  init = () => {
+  let init = () => {
     document.addEventListener('DOMContentLoaded', function () {
-      const select = document.querySelectorAll('select');
-      const instance = M.FormSelect.init(select, {});
-
-      // Rather not have done this but solves an issue with options
-      // having loads of white space around their text and glitching out.
-      const options = Array.from(document.querySelectorAll("option"));
-      options.map(o => {
-        o.textContent = o.textContent.trim();
-      });
-
-      const dd = instance[0].dropdown;
-      const selectableOptions = Array.from(dd.el.nextSibling.children);
-      const selectedIndex = selectableOptions.findIndex(
-          x => x.classList.contains("selected"));
-      selectableOptions[selectedIndex === 0 ? 1 : 0].click();
-      selectableOptions[selectedIndex].click();
       M.FloatingActionButton.init(
-          document.querySelectorAll('.fixed-action-btn'), options);
+          document.querySelectorAll('.fixed-action-btn'), {});
 
-      function registerButton(buttonId) {
+      let registerButton = (buttonId) => {
         const button = document.getElementById(buttonId);
-        button.onclick = function () {
+        button.onclick = () => {
           const form = document.getElementById('process-form');
           const element = document.createElement("input");
           element.name = "method";
@@ -31,8 +15,8 @@
           element.hidden = true;
           form.appendChild(element);
           form.submit();
-        }
-      }
+        };
+      };
 
       registerButton('approve-all');
       registerButton('approve-selection');
